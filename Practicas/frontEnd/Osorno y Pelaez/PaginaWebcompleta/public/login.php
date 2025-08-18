0f6143e433219c3cbc621f9c897fb7a01598fa7b
JuanOsorner <juanpowerlifting1000kg@gmail.com>
2025-08-18 12:54:57 -0500

    feat subiendo practicas al main

--- Practicas/frontEnd/Osorno y Pelaez/PaginaWebcompleta/public/login.php
+++ Practicas/frontEnd/Osorno y Pelaez/PaginaWebcompleta/public/login.php
@@ -4,14 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - Krypton Inventory</title>
-    <link rel="stylesheet" href="../../../public/assets/css/style.css"> 
+    <link rel="stylesheet" href="../public/assets/css/style.css"> 
 </head>
 <body>
 
     <div class="auth-container">
         <div class="auth-wrapper">
             <div class="logo-container">
-                <img src="../../../public/assets/img/logoKrip.png" alt="Logo Krypton Inventory">
+                <img src="../public/assets/img/logoKrip.png" alt="Logo Krypton Inventory">
             </div>
 
             <form id="login-form" action="controller/login_controller.php" method="POST">
